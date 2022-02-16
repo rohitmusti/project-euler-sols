7bfe0fe45df1f2a58b45464a502dbc848bd3a9a6
rohitmusti <rohit.musti.rm@gmail.com>
2022-02-15 22:32:35 -0500

    moved tests into their own file for readability for p03

--- p03/src/main.rs
+++ p03/src/main.rs
@@ -5,14 +5,16 @@
  * What is the largest prime factor of the number 600851475143 ?
  *
  * Solution:
- * Loop from 1 to the square root of the target number. Divide number by the 
- * whatever values you enounter that it evenly divides into, these will be its 
- * prime factors. By starting at the smallest number and continuing until reach the 
- * top, you guarantee that you will reduce the target number to its larges prime 
- * factor. This algorithm runs in O(sqrt(N)) where n is the target. This is the 
- * minimum possible solution as the prime factor could be its square root and 
+ * Loop from 1 to the square root of the target number. Divide number by the
+ * whatever values you enounter that it evenly divides into, these will be its
+ * prime factors. By starting at the smallest number and continuing until reach the
+ * top, you guarantee that you will reduce the target number to its larges prime
+ * factor. This algorithm runs in O(sqrt(N)) where n is the target. This is the
+ * minimum possible solution as the prime factor could be its square root and
  * must consider up until and including its square root.
  */
+
+mod test;
 
 fn main() {
     let target: u64 = 600_851_475_143;
@@ -31,13 +33,4 @@ fn largest_prime_factor(target: u64) -> u64 {
     }
 
     sol
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn basic_test() {
-        use crate::largest_prime_factor;
-        assert_eq!(largest_prime_factor(13_195), 29);
-    }
 }
